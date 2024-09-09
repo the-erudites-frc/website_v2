dd17010b09c3c3c768deaa2d3c77f7266de5a7e8
WoodIntegral888 <165996010+the-erudites-frc@users.noreply.github.com>
2024-09-09 09:46:33 -0400

    Footer, sponsor page
    - Mostly finished version of footer (does not have functional links yet)
    - Sponsors page completed (with quick thanks, types of sponsors, and link to email and more informations (the sponsorship package).

--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
+//function for header to shrink as page scroll down and grow again when scroll up
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    console.log("Entered1")
+    document.getElementById("header_div").style.paddingTop = "0.5%";
+    document.getElementById("header_title").style.fontSize = "300%";
+  } else {
+    console.log("Entered2")
+    document.getElementById("header_div").style.paddingTop = "3.5%";
+    document.getElementById("header_title").style.fontSize = "400%";
+  }
+}
+
+function show_below(assignment) {
+  var x = document.getElementById(assignment);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } 
+  else {
+    x.style.display = "none";
+  }
+}
+
+
 $(document).ready(function(){
+  //Function for overlay/loading screen between pages
   setTimeout(function endLoad(){
     $('body').addClass('loaded');
   }, 800);
@@ -6,17 +31,6 @@ $(document).ready(function(){
   window.onscroll = function() {scrollFunction()};
 
 
-  function scrollFunction() {
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-      console.log("Entered1")
-      document.getElementById("header_div").style.paddingTop = "0.5%";
-      document.getElementById("header_title").style.fontSize = "300%";
-    } else {
-      console.log("Entered2")
-      document.getElementById("header_div").style.paddingTop = "3.5%";
-      document.getElementById("header_title").style.fontSize = "400%";
-    }
-  }
   
   
 })
